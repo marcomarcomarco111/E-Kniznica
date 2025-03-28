770c6a43ed1936e58851f1c5d197a2064d34b3ca
marcomarcomarco111 <marcogundait@gmail.com>
2025-03-28 15:20:24 -0700

    Pridanie priečinku parts
    Pridal som priečinok parts do ktorého som dale Header,Footer,Login súbory.
    Ďalej som pridal 2 podstránky kosik a oblubene.

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>E-kniznica Bookly</title>
 
-    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
-
-    <!-- font awesome cdn link  -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-
-    <!-- custom css file link  -->
-    <link rel="stylesheet" href="css/style.css">
-
-</head>
 <body>
     
 <!-- header section starts  -->
-<?php require_once 'header.html'; ?>
+<?php require_once 'parts/header.html'; ?>
 <!-- header section ends -->
 
 
@@ -34,7 +20,7 @@
 </nav>
 
 <!-- login form  -->
-<?php require_once 'login.html'; ?>
+<?php require_once 'parts/login.html'; ?>
 
 
 <!-- home section starts  -->
@@ -686,34 +672,9 @@
 <!-- blogs section ends -->
 
 <!-- footer section starts  -->
-<?php require_once 'footer.html'; ?>
+<?php require_once 'parts/footer.html'; ?>
 <!-- footer section ends -->
 
 <!-- loader  -->
-
-<div class="loader-container">
-    <img src="image/loader-img.gif" alt="">
-</div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
-
-<!-- custom js file link  -->
-<script src="js/script.js"></script>
-
 </body>
 </html>
